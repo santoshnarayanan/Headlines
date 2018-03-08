ff3de73443075cdd57e74ed6a06b59d9e77d9eba
Santosh1978 <santosh33@gmail.com>
2018-03-09 01:51:26 +0530

    POST request
    Signed-off-by: Santosh1978 <santosh33@gmail.com>

--- headlines.py
+++ headlines.py
@@ -1,6 +1,7 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,11 +11,15 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
              'iol':'http://www.iol.co.za/cmlink/1.640'
              }
 
-@app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
-    feed  = feedparser.parse(RSS_FEEDS[publication])
-    # first_article = feed['entries'][0]
+@app.route("/",methods=['GET','POST'])
+def get_news():
+    query = request.form.get("publication")
+
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html",articles=feed['entries'])
 
 
