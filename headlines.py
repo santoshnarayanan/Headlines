7fe39aa30e3f2ceec9e3bd34a565dc6be4765429
Santosh1978 <santosh33@gmail.com>
2018-03-09 01:14:52 +0530

    Hyperlinks added to news

--- headlines.py
+++ headlines.py
@@ -1,6 +1,6 @@
 import feedparser
-
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -10,21 +10,13 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
              'iol':'http://www.iol.co.za/cmlink/1.640'
              }
 
-
 @app.route("/")
 @app.route("/<publication>")
-
 def get_news(publication="bbc"):
     feed  = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1>headlines </h1>
-            <b>{0}</b> </br>
-            <i>{1}</i> </br>
-            <p>{2}</p> </br>
-        </body>
-    </html>""".format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
+    # first_article = feed['entries'][0]
+    return render_template("home.html",articles=feed['entries'])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=5000, debug=True)
